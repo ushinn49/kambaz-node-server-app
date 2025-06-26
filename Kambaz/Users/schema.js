f9ee8e025e380110411fa0d7aa2c415edaec1531
Yuchen Cao <146129989+ushinn49@users.noreply.github.com>
2025-06-26 21:21:11 +0800

    Update schema.js

--- Kambaz/Users/schema.js
+++ Kambaz/Users/schema.js
@@ -1,16 +1,20 @@
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
-    _id: String,
+    _id: { 
+        type: String, 
+        required: true, 
+        default: () => new mongoose.Types.ObjectId().toString() 
+    },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    firstName: String,
-    lastName: String,
-    email: String,
-    dob: Date,
+    firstName: { type: String, default: "" },
+    lastName: { type: String, default: "" },
+    email: { type: String, default: "" },
+    dob: { type: Date, default: null },
     role: {
         type: String,
         enum: ["STUDENT", "FACULTY", "ADMIN", "USER"],
-        default: "USER",
+        default: "STUDENT",
     },
     loginId: String,
     section: String,

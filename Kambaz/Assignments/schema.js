217a26c62732d1ae4b6f24a4784d6536cf6ea491
Yuchen Cao <146129989+ushinn49@users.noreply.github.com>
2025-06-26 21:44:45 +0800

    Update schema.js

--- Kambaz/Assignments/schema.js
+++ Kambaz/Assignments/schema.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 const assignmentSchema = new mongoose.Schema({
-    _id: String,
-    title: String,
-    course: { type: String, ref: "CourseModel" },
-    description: String,
-    points: Number,
-    dueDate: Date,
-    availableDate: Date,
-    availableUntil: Date
+    _id: { 
+        type: String, 
+        required: true, 
+        default: () => new mongoose.Types.ObjectId().toString() 
+    },
+    title: { type: String, required: true, default: "New Assignment" },
+    course: { type: String, ref: "CourseModel", required: true },
+    description: { type: String, default: "" },
+    points: { type: Number, default: 100 },
+    dueDate: { type: Date, default: () => new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) }, // 一周后
+    availableDate: { type: Date, default: () => new Date() },
+    availableUntil: { type: Date, default: () => new Date(Date.now() + 14 * 24 * 60 * 60 * 1000) } // 两周后
 }, { collection: "assignments" });
 export default assignmentSchema;
